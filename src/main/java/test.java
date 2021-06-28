dbbb89d8675b7ecf79eca043e6d538d3c0a8b21a
cmh <cmh@163.com>
2021-06-28 18:41:15 +0800

    second

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,5 +5,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello 2");
+        System.out.println("hello 2");
     }
 }
